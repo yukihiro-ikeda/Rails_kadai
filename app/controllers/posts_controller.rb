04d65beded35a0bfa8817182ece83b9aa2c3a21c
yukihiro ikeda <headlessrecord@gmail.com>
2022-10-02 00:18:39 +0900

    createからnewへのリダイレクト機能を追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,5 +3,11 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    Post.create(content: params[:post][:content])
+    redirect_to new_post_path
   end
 end
