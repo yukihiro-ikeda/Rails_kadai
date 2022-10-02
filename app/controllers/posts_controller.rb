7778a4e948679ed1f3533a6fcac484887f908ff3
yukihiro ikeda <headlessrecord@gmail.com>
2022-10-02 11:18:10 +0900

    つぶやき削除機能を追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,11 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update,:destroy]
+
   def index
     @posts = Post.all
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def new
@@ -12,11 +13,9 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
     if @post.update(post_params)
       redirect_to posts_path, notice: "ブログを編集しました！"
     else
@@ -25,7 +24,6 @@ class PostsController < ApplicationController
   end
 
   def create
-    Post.create(content: params[:post][:content])
     @post = Post.new(post_params)
     if @post.save
     redirect_to new_post_path
@@ -34,9 +32,18 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    @post.destroy
+    redirect_to posts_path, notice:"つぶやきを削除しました！"
+  end
+
   private
 
   def post_params
     params.require(:post).permit(:content)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
